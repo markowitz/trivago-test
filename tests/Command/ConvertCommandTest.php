2d5b3881c1bcca049e3d80d60f5452f5c0877a50
Ikechukwu Nwakanma <icnwakanma@gmail.com>
2020-11-22 22:14:19 +0100

    update test

--- tests/Command/ConvertCommandTest.php
+++ tests/Command/ConvertCommandTest.php
@@ -2,10 +2,13 @@
 
 namespace App\Tests\Command;
 
+use App\Services\FileReader;
+use App\Services\FindFile;
+use App\Services\Validator;
 use Symfony\Bundle\FrameworkBundle\Console\Application;
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 use Symfony\Component\Console\Tester\CommandTester;
-
+use Symfony\Component\Finder\Finder;
 
 class ConvertCommandTest extends KernelTestCase
 {
@@ -26,7 +29,10 @@ class ConvertCommandTest extends KernelTestCase
         $this->assertStringContainsString('File generated successfully!', $output);
     }
 
-    public function testForWrongFilename()
+    /**
+     * test if file doesn't exists and execution fails
+     */
+    public function testExecuteFailsIfFileDoesNotExist()
     {
         $kernel = static::createKernel();
         $application = new Application($kernel);
@@ -34,13 +40,45 @@ class ConvertCommandTest extends KernelTestCase
         $command = $application->find('trivago:convert');
         $commandTester = new CommandTester($command);
         $commandTester->execute([
-            'input_file' => 'hotelsjson.xml',
+            'input_file' => 'hotels.json.xml',
         ]);
 
         // the output of the command in the console
-        $output = $commandTester->getErrorOutput();
+        $output = $commandTester->getDisplay();
 
-        $this->assertStringContainsString('Input File not found!', $output);
+        $this->assertStringNotContainsString('File generated successfully!', $output);
+    }
+
+    public function testFileSavesToCorrectFolder()
+    {
+        $kernel = static::createKernel();
+        $application = new Application($kernel);
+
+        $command = $application->find('trivago:convert');
+        $commandTester = new CommandTester($command);
+        $commandTester->execute([
+            'input_file' => 'hotels.xml',
+        ]);
+
+        $this->assertFileExists("var/out/hotels_xml.csv");
+
+    }
+
+    public function testValidation()
+    {
+        $hotel = [
+                "name" => "Apartment Dörr",
+                "address" => "Bolzmannweg 451, 05116 Hannoverßå",
+                "stars" => "-5",
+                "contact" => "Scarlet Kusch-Linke",
+                "phone" => "08177354570",
+                "uri" => "http://www.gar"
+                ];
+
+         $errors = Validator::validate($hotel);
+
+        $this->assertTrue(count($errors) > 0);
+
     }
 }
 
